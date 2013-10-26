a2abb3ad6d4c7387bf7c82298577f005d7989523
J Connolly <jsconnolly1@gmail.com>
2013-10-26 13:25:15 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeadingAge::Application.config.secret_key_base = 'c503073c6e90890bd6b0decdb8a94b9899b9e258aa2cc4554e7241231423269a82d29646beefee1c5d74b7266bc640be72c76854b64f3ca1b3b876a61fb6e17f'
